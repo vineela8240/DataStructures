0cf5076369742ce987004f538e56d597a5fccc24
Bhudati Vineela <Bhudati.Vineela@godigit.com>
2024-07-29 16:29:02 +0530

    Implementation of finding the square root of a number computed upto a specific precision

--- binarysearch/squarerootnumber.java
+++ binarysearch/squarerootnumber.java
@@ -1,2 +1,34 @@
-package binarysearch;public class squarerootnumber {
+package binarysearch;
+
+public class squarerootnumber {
+    public static double findSquareRoot(int number, double precision) {
+
+
+        double left = 0;
+        double right = number;
+        double mid = 0;
+
+        while ((right - left) > precision) {
+            mid = left + (right - left) / 2;
+            double midSquared = mid * mid;
+
+            if (midSquared == number) {
+                return mid;
+            } else if (midSquared < number) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+
+        return (left + right) / 2;
+    }
+
+    public static void main(String[] args) {
+        int number = 50;
+        double precision = 0.00001;
+        double sqrt = findSquareRoot(number, precision);
+        System.out.println("The square root of " + number + " is approximately: " + sqrt);
+    }
+
 }
