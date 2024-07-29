5e6ec521f0219cd71b51e6ed7d2eb65ca586c089
Bhudati Vineela <Bhudati.Vineela@godigit.com>
2024-07-29 16:23:33 +0530

    Implementation of checking the target element exists in a rotated sorted array

--- binarysearch/rotatedsortedarraytargetelement.java
+++ binarysearch/rotatedsortedarraytargetelement.java
@@ -1,2 +1,44 @@
-package binarysearch;public class rotatedsortedarraytargetelement {
+package binarysearch;
+
+public class rotatedsortedarraytargetelement {
+    public static boolean RotatedSortedArrayTargetElement(int[] num,int target){
+        int left=0;
+        int right=num.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(num[mid]==target){
+                return true;
+            }
+            //Check if left side is sorted
+            if(num[left]<=num[mid]){
+                //Target is in the left half
+                if(num[left]<=target&&target<num[mid]){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
+
+            }
+            else{
+                //Check if right half is sorted
+                if(num[mid]<target&&target<=num[right]){
+                    //Tartet is in right half
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int num[]={4,5,6,6,0,1,2};
+        int target=0;
+        System.out.println(RotatedSortedArrayTargetElement(num,target));
+        target=3;
+        System.out.println();
+    }
 }
